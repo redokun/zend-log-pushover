27e65a6d0c1a71620aaef420fe9766449cb94d10
Paolo Agostinetto <paul.ago@gmail.com>
2017-03-08 12:16:25 +0100

    First commit

---
+++ src/ZendLogPushover/PushoverLoggerException.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paul
+ * Date: 3/8/17
+ * Time: 10:21 AM
+ */
+
+namespace ZendLogPushover;
+
+
+/**
+ * Class ZendLogPushover${NAME}
+ * Date: 08/03/2017
+ * @author Paolo Agostinetto <paul.ago@gmail.com>
+ */
+class PushoverLoggerException {
+
+}
---
+++ src/ZendLogPushover/Writer/Pushover.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: paul
+ * Date: 3/8/17
+ * Time: 10:05 AM
+ */
+
+namespace ZendLogPushover\Writer;
+
+
+/**
+ * Class ZendLogPushover\Writer${NAME}
+ * Date: 08/03/2017
+ * @author Paolo Agostinetto <paul.ago@gmail.com>
+ */
+class Pushover extends \Zend\Log\Writer\AbstractWriter {
+
+	protected function doWrite(array $event) {
+		// TODO: Implement doWrite() method.
+	}
+}
