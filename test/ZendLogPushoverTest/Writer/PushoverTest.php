ce8c94c0022a43b7181b46cbbec557e9d7307e7b
Paolo Agostinetto <paul.ago@gmail.com>
2017-03-09 12:25:27 +0100

    Added a few basic unit tests

---
+++ test/ZendLogPushoverTest/Writer/PushoverTest.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace ZendLogPushoverTest\Writer;
+
+use Pushy\Message;
+use Zend\Log\Logger;
+use ZendLogPushover\Writer\Pushover;
+
+class PushoverTest extends \PHPUnit_Framework_TestCase {
+
+	/**
+	 * @expectedException \ZendLogPushover\PushoverLoggerException
+	 */
+	public function testShouldFailBecauseMissingUser() {
+
+		/** @var \Pushy\Client $client */
+		$client = $this->getMockBuilder(\Pushy\Client::class)
+			->disableOriginalConstructor()
+			->getMock();
+
+		$writer = new Pushover();
+		$writer->setClient($client);
+
+		$this->invokeMethod($writer, "doWrite", [$this->getEventData()]);
+	}
+
+	/**
+	 * @expectedException \ZendLogPushover\PushoverLoggerException
+	 */
+	public function testShouldFailBecauseMissingClient() {
+
+		$user = $this->getUserMock();
+
+		$writer = new Pushover();
+		$writer->setUser($user);
+
+		$this->invokeMethod($writer, "doWrite", [$this->getEventData()]);
+	}
+
+	public function testShouldWork() {
+
+		$client = $this->getClientMock();
+		$user = $this->getUserMock();
+
+		$client->expects($this->once())
+			->method('sendMessage')
+			->with($this->isInstanceOf(Message::class));
+
+		$writer = new Pushover();
+		$writer->setUser($user);
+		$writer->setClient($client);
+
+		$this->invokeMethod($writer, "doWrite", [$this->getEventData()]);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getEventData() {
+		return [
+			'timestamp' => new \DateTime(),
+			'priority' => Logger::DEBUG,
+			'message' => "Test entry",
+		];
+	}
+
+	/**
+	 * @return \PHPUnit_Framework_MockObject_MockObject|\Pushy\User
+	 */
+	public function getUserMock() {
+		$user = $this->getMockBuilder(\Pushy\User::class)
+			->disableOriginalConstructor()
+			->getMock();
+
+		return $user;
+	}
+
+	/**
+	 * @return \PHPUnit_Framework_MockObject_MockObject|\Pushy\Client
+	 */
+	public function getClientMock() {
+		$client = $this->getMockBuilder(\Pushy\Client::class)
+			->disableOriginalConstructor()
+			->getMock();
+
+		return $client;
+	}
+
+	/**
+	 * Call protected/private method of a class.
+	 *
+	 * @param object &$object Instantiated object that we will run method on.
+	 * @param string $methodName Method name to call
+	 * @param array $parameters Array of parameters to pass into method.
+	 *
+	 * @return mixed Method return.
+	 */
+	public function invokeMethod(&$object, $methodName, array $parameters = array()) {
+		$reflection = new \ReflectionClass(get_class($object));
+		$method = $reflection->getMethod($methodName);
+		$method->setAccessible(true);
+
+		return $method->invokeArgs($object, $parameters);
+	}
+}
